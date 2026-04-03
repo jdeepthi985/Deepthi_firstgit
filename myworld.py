cacf8912501c8b818995f5b78fff67e706ead8be
jdeepthi985 <jdeepthi985@gmail.com>
2026-04-03 23:02:46 +0530

    motivation

--- myworld.py
+++ myworld.py
@@ -3,3 +3,7 @@ Print("Hello, World!" Changes made in Local machine. changes made in local, anot
 karthik ram came here and he is a good boy)
 pull from remote . see the changes in local
 one more to remote. practise makes man perfect
+you can do it karthik ram. you are a gem of a person. keep going and never give up. you will achieve great things in 
+life. believe in yourself and your abilities. you have the potential to do amazing things. keep pushing forward and
+never stop learning. success is just around the corner, so keep working hard and stay focused on your goals.
+you got this!
